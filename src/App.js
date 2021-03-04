7f204461e4af8b950b5eae4c139bad21055f8586
Pradeep462 <Pradeepbhadauria462002@bbdu.ac.in>
2021-03-04 19:10:32 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import CityInput from './components/CityInput/index'
+import CityWeather from './components/CityWeather/index'
 
 function App() {
+
+     
+  const [city , setCity] = React.useState("");
+  const [cityWeather , setCityWeather] = React.useState({});
+
+  const fetchCityWeather = () =>{
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a68129f8765f3665c9b7a9e4ce25cc51`
+      )
+      .then((response)=> response.json())
+      .then((result) => {
+        setCityWeather(result);
+      })
+      .catch((error)=>{
+        console.log("error is",error);
+      })
+  };
+
+ // console.log(cityWeather.main.temp);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container bg-dark text-center text-uppercase font-weight-bold rounded" style={{fontSize:"25px", color:"white"}}>
+    <CityInput city={city} setCity={setCity} fetchCityWeather={fetchCityWeather}/>
+    <CityWeather cityWeather={cityWeather}/>
     </div>
   );
 }
